3dc9b77cc3fc5d5eb1c9013c926bc542c50bbf5d
Bruna Drago <bruna.drago@ifood.com.br>
2024-09-03 09:23:32 -0300

    setup

---
+++ SA2App.swift
@@ -0,0 +1,17 @@
+//
+//  SA2App.swift
+//  SA2
+//
+//  Created by Bruna Drago on 03/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SA2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
